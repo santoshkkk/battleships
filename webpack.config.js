14e6cf835bf72bec8c86ec26f047d7caa94b894b
adwaitpawar <adwaitpawar@gmail.com>
2024-02-09 15:27:21 +0530

    Your descriptive commit message here

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './server.js', // Update the entry point to server.js
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // Add other configuration options as needed
+};
